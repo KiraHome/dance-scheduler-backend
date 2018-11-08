3e4d07c95cea92ae9514f047c284fa744482dc0b
Daniel Preszl <daniel.preszl@ge.com>
2018-11-08 16:21:59 +0100

    Implement email sending service for events

--- routes/timetable.js
+++ routes/timetable.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const client = require('../database/handler');
+const mailSender = require('../utils/email-sender');
 
 router.get('/', async (req, res) => {
     try {
@@ -20,6 +21,10 @@ router.put('/', async (req, res) => {
             [body.start, body.end, body.title, body.color, body.id, body.cssClass, false]
         );
 
+        const start = new Date(body.start).toLocaleString('HU-hu');
+        const end = new Date(body.end).toLocaleString('HU-hu');
+        mailSender.sendMail('<pre>' + body.title + ' ' + start + '-tól ' + end + '-ig</pre>', body.title, 'preszl.daniel@gmail.com');
+
         res.send({serial_id: result.rows[0].serial_id});
     } catch (e) {
         console.log(e.stack);
@@ -28,10 +33,17 @@ router.put('/', async (req, res) => {
 });
 
 router.delete('/*', async (req, res) => {
+    const serialId = req.path.substring(1);
     try {
-        await client.query('DELETE FROM timetable WHERE serial_id=' + req.path.substring(1));
+        const result = await client.query('SELECT start, end_, title FROM timetable WHERE serial_id=' + serialId);
+        await client.query('DELETE FROM timetable WHERE serial_id=' + serialId);
 
-        res.send({serial_id: req.path.substring(1)});
+        const start = new Date(result.rows[0].start).toLocaleString('HU-hu');
+        const end = new Date(result.rows[0].end_).toLocaleString('HU-hu');
+        mailSender.sendMail('<pre>' + result.rows[0].title + ' ' + start + '-tól ' + end + '-ig <b>TÖRÖLVE</b> </pre>',
+            result.rows[0].title + ' - törölve', 'preszl.daniel@gmail.com');
+
+        res.send({serial_id: serialId});
     } catch (e) {
         console.log(e.stack);
         res.sendStatus(400);
